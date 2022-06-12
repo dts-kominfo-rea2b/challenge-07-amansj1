dac4742e7a101e6c17089009bf38baff179b849a
amansj1 <51205338+amansj1@users.noreply.github.com>
2022-06-12 22:57:17 +0700

    marah tidak marah

--- promise.js
+++ promise.js
@@ -1,7 +1,29 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const counter = (data, hasil) =>{
+  var count =  0;
+  data.forEach(film => {
+    if(film.hasil == hasil)
+    count++;
+  });
+  return count;
+}
+
+const promiseOutput = (hasil) =>{
+  return new Promise((p) =>{
+    promiseTheaterIXX().then((IXX)=>{
+      var countIxx = counter(IXX, hasil);
+
+      promiseTheaterVGC().then((VGC)=>{
+        var countVgc = counter(VGC, hasil);
+        p(countIxx + countVgc)
+      })
+    })
+  });
+};
+
+
 
 module.exports = {
   promiseOutput,
